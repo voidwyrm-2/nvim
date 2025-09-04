42abb46b9bb49348674f7ade5448fee5b3b6ea5d
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2025-09-04 00:16:42 -0400

    Added .nps -> postscr highlighting override

--- init.lua
+++ init.lua
@@ -2,7 +2,14 @@ require("voidwyrm-2.lazy")
 require("voidwyrm-2.core")
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.cmd("cd %:p:h")
-  end,
+    callback = function()
+        vim.cmd("cd %:p:h")
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = ".nps",
+    callback = function()
+        vim.cmd("set ft=postscr")
+    end
 })
